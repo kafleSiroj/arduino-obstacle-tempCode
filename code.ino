65097c181c21d1df3b6a9a59ee5f6ff4b17c36bd
Siroj K <167022994+kafleSiroj@users.noreply.github.com>
2024-10-25 22:19:48 +0545

    code.ino

--- code.ino
+++ code.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 int m1p1 = 2;
 int m1p2 = 3;
 int ena1 = 1; 
@@ -7,28 +9,39 @@ int ena2 = 4;
 int echo = 12;
 int trig = 13;
 long duration;
-int distance; 
+int distance(); 
+
+Servo myServo;
+
+void stop();
+void forward();
+void reverseLeft();
+void reverseRight();
+void reverse();
+
+void turnLeft();
+void turnRight();
 
 void setup() {
+  pinMode(ena1, OUTPUT);
   pinMode(m1p1, OUTPUT);
   pinMode(m1p2, OUTPUT);
+  pinMode(ena2, OUTPUT);
   pinMode(m2p1, OUTPUT);
   pinMode(m2p2, OUTPUT);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
+
+  myServo.attach(11);
+  myServo.write(90);
 }
 
-void loop() {
+void stop() {
+  analogWrite(ena1, 0);
+  analogWrite(ena2, 0);
+}
 
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(2);
-  digitalWrite(trig, LOW);
-  delayMicroseconds(10);
-
-  duration = pulseIn(echo, HIGH);
-
-  distance = duration * 0.032 / 2;
-
+void forward() {
   digitalWrite(m1p1, HIGH);
   digitalWrite(m1p2, LOW);
   analogWrite(ena1, 255);
@@ -36,16 +49,95 @@ void loop() {
   digitalWrite(m2p1, HIGH);
   digitalWrite(m2p2, LOW);
   analogWrite(ena2, 255);
+}
 
-  if (distance <= 10) {
-    digitalWrite(m1p1, LOW);
-    digitalWrite(m1p2, HIGH);
-    analogWrite(ena1, 255);
+void reverse() {
+  digitalWrite(m1p1, LOW);
+  digitalWrite(m1p2, HIGH);
+  analogWrite(ena1, 255);
 
-    digitalWrite(m2p1, LOW);
-    digitalWrite(m2p2, HIGH);
-    analogWrite(ena2, 255);
+  digitalWrite(m2p1, LOW);
+  digitalWrite(m2p2, HIGH);
+  analogWrite(ena2, 255);
+}
+
+void reverseLeft() {
+  digitalWrite(m2p1, LOW);
+  digitalWrite(m2p2, HIGH);
+  analogWrite(ena2, 255);
+
+  analogWrite(ena1, 0);
+    
+}
+
+void reverseRight() {
+  digitalWrite(m1p1, LOW);
+  digitalWrite(m1p2, HIGH);
+  analogWrite(ena1, 255);
+
+  analogWrite(ena2, 0);
+}
+
+int distance() {
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  duration = pulseIn(echo, HIGH);
+
+  return duration * 0.034 / 2;
+}
+
+void turnLeft() {
+  stop();
+  delay(1000);
+
+  reverse();
+  delay(1000);
+
+  stop();
+  delay(500);
+
+  reverseLeft();
+  delay(1000);
+    
+  stop();
+  delay(500);
+}
+
+void turnRight() {
+  stop();
+  delay(1000);
+
+  reverse();
+  delay(1000);
+
+  stop();
+  delay(500);
+
+  reverseRight();
+  delay(1000);
+  
+  stop();
+  delay(500);
+}
+
+void loop() {
+  int Fdistance = distance();
+
+  forward();
+  
+  if (Fdistance <= 10) {
+    turnLeft();
   }
+
+
+  else {
+    myServo.write(90);
+  }
+
+  delay(100);
+
 }
 
 
